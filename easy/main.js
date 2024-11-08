09570030c826ce44d495b0e78939aa8167dab87d
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-08 17:12:58 +0500

    Initial commit

---
+++ easy/main.js
@@ -0,0 +1,19 @@
+// 1) Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return []; // Return empty array if there is no solution
+}
+
+let array = [2, 7, 11, 15];
+let target = 9;
+console.log(twoSum(array, target));
