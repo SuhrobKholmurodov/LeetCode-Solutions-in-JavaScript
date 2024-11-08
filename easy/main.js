ec83a5b783a1738bbb4a3a3d33aecddb12c031b3
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-09 01:27:27 +0500

    added searchInsert function #5

--- easy/main.js
+++ easy/main.js
@@ -53,7 +53,6 @@ function longestCommonPrefix(strs) {
 let strings = ["flower", "flow", "flight"];
 // console.log(longestCommonPrefix(strings));
 
-
 // 4) Given two strings needle and haystack, return the index of the first occurrence of
 // needle in haystack, or -1 if needle is not part of haystack.
 // Input: haystack = "sadbutsad", needle = "sad"
@@ -75,4 +74,30 @@ function strStr(word, str) {
   }
   return -1;
 }
-// console.log(strStr("mississippi", "issi")); 
+// console.log(strStr("mississippi", "issi"));
+
+// Given a sorted array of distinct integers and a target value, return the index if the target
+// is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+function searchInsert(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (arr[middle] == target) {
+      return middle;
+    }
+    if (arr[middle] < target) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return left;
+}
+let array1 = [1, 3, 5, 6];
+let target1 = 5;
+console.log(searchInsert(array1, target1));
