48fd5ad395fa8e14467197c1c114eedb03d7c52f
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-09 02:00:30 +0500

    added climbStairs function #7

--- easy/main.js
+++ easy/main.js
@@ -112,4 +112,31 @@ function lengthOfLastWord(str) {
   let strSplit = str.trim().split(" ");
   return strSplit[strSplit.length - 1].length;
 }
-console.log(lengthOfLastWord("   fly me   to   the moon  "));
+// console.log(lengthOfLastWord("   fly me   to   the moon  "));
+
+
+// 7) You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+function climbStairs(n) {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  let first = 1;
+  let second = 2;
+  let total;
+
+  for (let i = 3; i <= n; i++) {
+    total = first + second;
+    first = second;
+    second = total;
+  }
+
+  return total;
+}
+// console.log(climbStairs(2));
