ae3a1bf9ae10e632e8f8cc8146cf3876bca4d52f
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-10 04:03:19 +0500

    added dominantIndex function #16

--- easy/main.js
+++ easy/main.js
@@ -280,3 +280,23 @@ function toLowerCase(str) {
   return str.toLowerCase();
 }
 // console.log(toLowerCase("Hello"));
+
+// 16) You are given an integer array nums where the largest integer is unique.
+// Determine whether the largest element in the array is at least twice as much as every other number in the array.
+// If it is, return the index of the largest element, or return -1 otherwise.
+// Input: nums = [3,6,1,0]
+// Output: 1
+// Explanation: 6 is the largest integer.
+// For every other number in the array x, 6 is at least twice as big as x.
+// The index of value 6 is 1, so we return 1.
+
+function dominantIndex(nums) {
+  const maxNum = Math.max(...nums);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== maxNum && maxNum < nums[i] * 2) {
+      return -1;
+    }
+  }
+  return nums.indexOf(maxNum);
+}
+// console.log(dominantIndex([1, 2, 3, 4]));
