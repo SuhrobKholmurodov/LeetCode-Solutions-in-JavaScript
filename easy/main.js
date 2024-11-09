1b22390b54f5d0dc8483cd40249f78d197fc1a33
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-10 03:36:43 +0500

    added search function #14

--- easy/main.js
+++ easy/main.js
@@ -251,3 +251,23 @@ function isPowerOfFour(num) {
   return num === 1;
 }
 // console.log(isPowerOfFour(16));
+
+// 14) Given an array of integers nums which is sorted in ascending order, and an integer target,
+//  write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+function search(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (array[mid] === target) return mid;
+    if (array[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+}
+// console.log(search([-1, 0, 3, 5, 9, 12], 9));
