3652b4b26631972991c0449c810f96a774a5910e
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-11 02:29:42 +0500

    added sleep function #20

--- easy/main.js
+++ easy/main.js
@@ -335,4 +335,23 @@ function defangIPaddr(str) {
 function isEmpty(obj) {
   return !Object.keys(obj).length;
 }
-console.log(isEmpty({ x: 5, y: 42 }));
+// console.log(isEmpty({ x: 5, y: 42 }));
+
+// 20) Given a positive integer millis, write an asynchronous function
+// that sleeps for millis milliseconds. It can resolve any value.
+// Input: millis = 100
+// Output: 100
+// Explanation: It should return a promise that resolves after 100ms.
+// let t = Date.now();
+// sleep(100).then(() => {
+// console.log(Date.now() - t); // 100
+// });
+
+async function sleep(millis) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+// let t = Date.now();
+// sleep(200).then(() => {
+//   console.log(Date.now() - t);
+// });
