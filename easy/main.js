d0c305318e440d2fb38254fbcb31c714ad976052
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-11 01:05:29 +0500

    added sortArrayByParity function #17

--- easy/main.js
+++ easy/main.js
@@ -300,3 +300,16 @@ function dominantIndex(nums) {
   return nums.indexOf(maxNum);
 }
 // console.log(dominantIndex([1, 2, 3, 4]));
+
+// 17) Given an integer array nums, move all the even integers at the beginning of the array
+// followed by all the odd integers. Return any array that satisfies this condition.
+// Input: nums = [3,1,2,4]
+// Output: [2,4,3,1]
+// Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+function sortArrayByParity(array) {
+  return array.sort((a, b) => {
+    return (a % 2) - (b % 2);
+  });
+}
+console.log(sortArrayByParity([3, 1, 2, 4]));
