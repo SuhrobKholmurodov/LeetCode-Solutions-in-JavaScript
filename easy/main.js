2fbbf2cc4e4be4dbf52014cb2978552b1010d41c
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-11 02:13:14 +0500

    added isEmpty function #19

--- easy/main.js
+++ easy/main.js
@@ -323,3 +323,16 @@ function defangIPaddr(str) {
   return str.replace(/\./g, "[.]");
 }
 // console.log(defangIPaddr("1.1.1.1"));
+
+// 19) Given an object or an array, return if it is empty.
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+// Input: obj = {"x": 5, "y": 42}
+// Output: false
+// Explanation: The object has 2 key-value pairs so it is not empty.
+
+function isEmpty(obj) {
+  return !Object.keys(obj).length;
+}
+console.log(isEmpty({ x: 5, y: 42 }));
