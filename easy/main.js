f7c658f5f5f91fdca60524f7df3f5d7a2911923f
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-11 04:10:26 +0500

    added plusOne function #25

--- easy/main.js
+++ easy/main.js
@@ -439,3 +439,30 @@ function createHelloWorld() {
 }
 const f = createHelloWorld();
 // console.log(f({}, null, 42));
+
+// 25) You are given a large integer represented as an integer array digits,
+// where each digits[i] is the ith digit of the integer. The digits are ordered
+// from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+function plusOne(array) {
+  let newArray = array.slice();
+
+  for (let i = newArray.length - 1; i >= 0; i--) {
+    if (newArray[i] < 9) {
+      newArray[i]++;
+      return newArray;
+    }
+    newArray[i] = 0;
+  }
+
+  newArray.unshift(1);
+  return newArray;
+}
+// console.log(plusOne([1, 2, 3]));
+// console.log(plusOne([9]));
