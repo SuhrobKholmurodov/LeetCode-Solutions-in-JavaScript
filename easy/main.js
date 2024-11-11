f9d00a08d2cbb7720e1d7effdccd0031982ce3f8
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-11 14:55:45 +0500

    added maximum69Number function #27

--- easy/main.js
+++ easy/main.js
@@ -496,3 +496,28 @@ function selfDividingNumbers(left, right) {
 }
 
 // console.log(selfDividingNumbers(1, 22));
+
+// 27) You are given a positive integer num consisting only of digits 6 and 9.
+// Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+// Input: num = 9669
+// Output: 9969
+// Explanation:
+// Changing the first digit results in 6669.
+// Changing the second digit results in 9969.
+// Changing the third digit results in 9699.
+// Changing the fourth digit results in 9666.
+// The maximum number is 9969.
+
+function maximum69Number(num) {
+  let strNum = num.toString();
+  let maxNum = num;
+  for (let i = 0; i < strNum.length; i++) {
+    if (strNum[i] === "6") {
+      let newStr = strNum.slice(0, i) + "9" + strNum.slice(i + 1);
+      let newNum = parseInt(newStr);
+      maxNum = Math.max(maxNum, newNum);
+    }
+  }
+  return maxNum;
+}
+// console.log(maximum69Number(9669));
