1a46efe5cdf77b28518a3768cf01dc3e94924646
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-11 14:12:29 +0500

    added selfDividingNumbers function #26

--- easy/main.js
+++ easy/main.js
@@ -466,3 +466,33 @@ function plusOne(array) {
 }
 // console.log(plusOne([1, 2, 3]));
 // console.log(plusOne([9]));
+
+// 26) A self-dividing number is a number that is divisible by every digit it contains.
+// For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+// A self-dividing number is not allowed to contain the digit zero.
+// Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right] (both inclusive).
+// Input: (left = 1), (right = 22);
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22];
+
+function selfDividingNumbers(left, right) {
+  let res = [];
+  for (let i = left; i <= right; i++) {
+    let num = i;
+    let isSelfDividing = true;
+
+    while (num > 0) {
+      let digit = num % 10;
+      if (digit === 0 || i % digit !== 0) {
+        isSelfDividing = false;
+        break;
+      }
+      num = Math.floor(num / 10);
+    }
+    if (isSelfDividing) {
+      res.push(i);
+    }
+  }
+  return res;
+}
+
+// console.log(selfDividingNumbers(1, 22));
