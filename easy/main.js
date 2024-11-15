f80b1c3d67452e52664b72a91627edf95a7862bb
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-15 15:44:59 +0500

    added sortSentence function #41

--- easy/main.js
+++ easy/main.js
@@ -806,3 +806,32 @@ function truncateSentence(str, k) {
   return res.join(" ");
 }
 // console.log(truncateSentence("Hello how are you Contestant", 4));
+
+// 41) A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// Each word consists of lowercase and uppercase English letters.
+// A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+// For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+// Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+// Input: s = "is2 sentence4 This1 a3"
+// Output: "This is a sentence"
+// Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+
+function sortSentence(str) {
+  let strSplit = str.split(" ");
+  let res = [];
+  for (let i = 0; i < strSplit.length; i++) {
+    let indStr = strSplit[i]
+      .split("")
+      .filter((el) => !isNaN(el))
+      .join("");
+    let index = Number(indStr);
+    res.push({ word: strSplit[i], index: index });
+  }
+  res.sort((a, b) => a.index - b.index);
+  return res
+    .map((el) => {
+      return el.word.slice(0, el.word.length - 1);
+    })
+    .join(" ");
+}
+// console.log(sortSentence("is2 sentence4 This1 a3"));
