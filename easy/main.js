847a5c3f7d784b47eb9aa751f2aae0fb2f906123
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-15 15:02:48 +0500

    added truncateSentence function #40

--- easy/main.js
+++ easy/main.js
@@ -784,3 +784,25 @@ function halvesAreAlike(str) {
   return cnt1 === cnt2;
 }
 // console.log(halvesAreAlike("textbook"));
+
+// 40) A sentence is a list of words that are separated by a single space with no leading
+//  or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+// For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+// You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it
+// contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+// Input: s = "Hello how are you Contestant", k = 4
+// Output: "Hello how are you"
+// Explanation:
+// The words in s are ["Hello", "how" "are", "you", "Contestant"].
+// The first 4 words are ["Hello", "how", "are", "you"].
+// Hence, you should return "Hello how are you".
+
+function truncateSentence(str, k) {
+  let res = [];
+  let strSplit = str.split(" ");
+  for (let i = 0; i < k; i++) {
+    res.push(strSplit[i]);
+  }
+  return res.join(" ");
+}
+// console.log(truncateSentence("Hello how are you Contestant", 4));
