5970e80774ef099e40a0df28c9479b9e8613e4c1
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-15 18:44:21 +0500

    added maximumDifference function #45

--- easy/main.js
+++ easy/main.js
@@ -888,3 +888,25 @@ function reversePrefix(word, ch) {
   return reversedPart1 + part2;
 }
 // console.log(reversePrefix("abcdefd", "d"));
+
+// 45) Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i]
+// and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+// Return the maximum difference. If no such i and j exists, return -1.
+// Input: nums = [7,1,5,4]
+// Output: 4
+// Explanation:
+// The maximum difference occurs with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+// Note that with i = 1 and j = 0, the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+
+function maximumDifference(nums) {
+  let res = [];
+  let minElem = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > minElem) {
+      res.push(nums[i] - minElem);
+    }
+    minElem = Math.min(minElem, nums[i]);
+  }
+  return res.length > 0 ? Math.max(...res) : -1;
+}
+// console.log(maximumDifference([7, 1, 5, 4]));
