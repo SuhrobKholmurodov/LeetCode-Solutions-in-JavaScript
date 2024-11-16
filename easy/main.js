b764d4eed7a2c19fc4c75189cc7d1ff2a53ed3c1
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-16 22:58:19 +0500

    added mostWordsFound function #46

--- easy/main.js
+++ easy/main.js
@@ -910,3 +910,30 @@ function maximumDifference(nums) {
   return res.length > 0 ? Math.max(...res) : -1;
 }
 // console.log(maximumDifference([7, 1, 5, 4]));
+
+// 46) A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+// Return the maximum number of words that appear in a single sentence.
+// Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+// Output: 6
+// Explanation:
+// - The first sentence, "alice and bob love leetcode", has 5 words in total.
+// - The second sentence, "i think so too", has 4 words in total.
+// - The third sentence, "this is great thanks very much", has 6 words in total.
+// Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+
+function mostWordsFound(array) {
+  let maxLength = 0;
+  for (let i = 0; i < array.length; i++) {
+    const wordCnt = array[i].split(" ").length;
+    maxLength = Math.max(maxLength, wordCnt);
+  }
+  return maxLength;
+}
+// console.log(
+//   mostWordsFound([
+//     "alice and bob love leetcode",
+//     "i think so too",
+//     "this is great thanks very much",
+//   ])
+// );
