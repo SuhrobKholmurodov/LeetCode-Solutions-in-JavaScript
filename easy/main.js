0483eac23cf0c53b1f2d64a33357bd0a5f5e1b07
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-16 23:10:07 +0500

    added isSameAfterReversals function #47

--- easy/main.js
+++ easy/main.js
@@ -937,3 +937,26 @@ function mostWordsFound(array) {
 //     "this is great thanks very much",
 //   ])
 // );
+
+// 47) Reversing an integer means to reverse all its digits.
+// For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+// Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2.
+// Return true if reversed2 equals num. Otherwise return false.
+// Input: num = 526
+// Output: true
+// Explanation: Reverse num to get 625, then reverse 625 to get 526, which equals num.
+
+function isSameAfterReversals(num) {
+  function reverseNum(n) {
+    let reversed = 0;
+    while (n > 0) {
+      reversed = reversed * 10 + (n % 10);
+      n = Math.floor(n / 10);
+    }
+    return reversed;
+  }
+  const reversed1 = reverseNum(num);
+  const reversed2 = reverseNum(reversed1);
+  return reversed2 === num;
+}
+// console.log(isSameAfterReversals(526));
