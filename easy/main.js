47836f236a5f632c513bd946887ba0b16aadc626
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-16 23:57:37 +0500

    added capitalizeTitle function #49

--- easy/main.js
+++ easy/main.js
@@ -983,3 +983,29 @@ function checkString(str) {
   return true;
 }
 // console.log(checkString("abab"));
+
+// 49) You are given a string title consisting of one or more words separated by a single space,
+//  where each word consists of English letters. Capitalize the string by changing the capitalization of each word such that:
+// If the length of the word is 1 or 2 letters, change all letters to lowercase.
+// Otherwise, change the first letter to uppercase and the remaining letters to lowercase.
+// Return the capitalized title.
+// Input: title = "First leTTeR of EACH Word"
+// Output: "First Letter of Each Word"
+// Explanation:
+// The word "of" has length 2, so it is all lowercase.
+// The remaining words have a length of at least 3, so the first letter of each 
+// remaining word is uppercase, and the remaining letters are lowercase
+
+function capitalizeTitle(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((el) => {
+      if (el.length > 2) {
+        return el.charAt(0).toUpperCase() + el.slice(1);
+      }
+      return el;
+    })
+    .join(" ");
+}
+console.log(capitalizeTitle("First leTTeR of EACH Word"));
