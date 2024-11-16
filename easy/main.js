59736a02ad5f2862ad30bb9cac36fb0b5388dda2
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-17 01:01:21 +0500

    added divideString function #50

--- easy/main.js
+++ easy/main.js
@@ -993,7 +993,7 @@ function checkString(str) {
 // Output: "First Letter of Each Word"
 // Explanation:
 // The word "of" has length 2, so it is all lowercase.
-// The remaining words have a length of at least 3, so the first letter of each 
+// The remaining words have a length of at least 3, so the first letter of each
 // remaining word is uppercase, and the remaining letters are lowercase
 
 function capitalizeTitle(str) {
@@ -1008,4 +1008,36 @@ function capitalizeTitle(str) {
     })
     .join(" ");
 }
-console.log(capitalizeTitle("First leTTeR of EACH Word"));
+// console.log(capitalizeTitle("First leTTeR of EACH Word"));
+
+// 50) A string s can be partitioned into groups of size k using the following procedure:
+// The first group consists of the first k characters of the string, the second group
+// consists of the next k characters of the string, and so on. Each character can be a part of exactly one group.
+// For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
+// Note that the partition is done so that after removing the fill character from the last group (if it exists)
+//  and concatenating all the groups in order, the resultant string should be s.
+// Given the string s, the size of each group k and the character fill, return a string array denoting the
+// composition of every group s has been divided into, using the above procedure.
+// Input: s = "abcdefghi", k = 3, fill = "x"
+// Output: ["abc","def","ghi"]
+// Explanation:
+// The first 3 characters "abc" form the first group.
+// The next 3 characters "def" form the second group.
+// The last 3 characters "ghi" form the third group.
+// Since all groups can be completely filled by characters from the string, we do not need to use fill.
+// Thus, the groups formed are "abc", "def", and "ghi".
+
+function divideString(str, k, fill) {
+  let res = [];
+  let strSplit = str.split("");
+  for (let i = 0; i < strSplit.length; i += k) {
+    res.push(strSplit.slice(i, k + i).join(""));
+  }
+  if (res[res.length - 1].length < k) {
+    while (res[res.length - 1].length < k) {
+      res[res.length - 1] += fill;
+    }
+  }
+  return res;
+}
+// console.log(divideString("abcdefghi", 3, "x"));
