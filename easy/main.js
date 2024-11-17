c9e87e9bff642039bbed71a541d36e64b7c415aa
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-18 03:50:55 +0500

    added digitSum function #56

--- easy/main.js
+++ easy/main.js
@@ -1142,3 +1142,38 @@ function findClosestNumber(array) {
   return minElem;
 }
 // console.log(findClosestNumber([-4, -2, 1, 4, 8]));
+
+// 56) You are given a string s consisting of digits and an integer k.
+// A round can be completed if the length of s is greater than k. In one round, do the following:
+// Divide s into consecutive groups of size k such that the first k characters are in the first group, 
+// the next k characters are in the second group, and so on. Note that the size of the last group can be smaller than k.
+// Replace each group of s with a string representing the sum of all its digits. For example, 
+// "346" is replaced with "13" because 3 + 4 + 6 = 13. Merge consecutive groups together to form a new string. 
+// If the length of the string is greater than k, repeat from step 1. Return s after all rounds have been completed.
+// Input: s = "11111222223", k = 3
+// Output: "135"
+// Explanation: 
+// - For the first round, we divide s into groups of size 3: "111", "112", "222", and "23".
+  // ​​​​​Then we calculate the digit sum of each group: 1 + 1 + 1 = 3, 1 + 1 + 2 = 4, 2 + 2 + 2 = 6, and 2 + 3 = 5. 
+  // So, s becomes "3" + "4" + "6" + "5" = "3465" after the first round.
+// - For the second round, we divide s into "346" and "5".
+  // Then we calculate the digit sum of each group: 3 + 4 + 6 = 13, 5 = 5. 
+  // So, s becomes "13" + "5" = "135" after second round. 
+// Now, s.length <= k, so we return "135" as the answer.
+
+function digitSum(str, k) {
+  let currentStr = str;
+  while (currentStr.length > k) {
+    let resArray = [];
+    for (let i = 0; i < currentStr.length; i += k) {
+      let group = currentStr.slice(i, i + k);
+      let sum = group
+        .split("")
+        .reduce((acc, digit) => acc + parseInt(digit), 0);
+      resArray.push(sum.toString());
+    }
+    currentStr = resArray.join("");
+  }
+  return currentStr;
+}
+// console.log(digitSum("11111222223", 3));
