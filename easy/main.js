41263b8abfc4fbba21a2113c49695220fa34e6ca
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-18 03:40:13 +0500

    added findClosestNumber function #55

--- easy/main.js
+++ easy/main.js
@@ -1114,3 +1114,31 @@ function sum(num1, num2) {
   return num1 + num2;
 }
 // console.log(sum(12, 5));
+
+// 55) Given an integer array nums of size n, return the number with the value closest to 0 in nums.
+// If there are multiple answers, return the number with the largest value.
+// Input: nums = [-4,-2,1,4,8]
+// Output: 1
+// Explanation:
+// The distance from -4 to 0 is |-4| = 4.
+// The distance from -2 to 0 is |-2| = 2.
+// The distance from 1 to 0 is |1| = 1.
+// The distance from 4 to 0 is |4| = 4.
+// The distance from 8 to 0 is |8| = 8.
+// Thus, the closest number to 0 in the array is 1.
+
+function findClosestNumber(array) {
+  let minElem = array[0];
+  for (let i = 0; i < array.length; i++) {
+    let moduleElem = Math.abs(array[i]);
+    let moduleClosest = Math.abs(minElem);
+    if (
+      moduleElem < moduleClosest ||
+      (moduleElem === moduleClosest && array[i] > minElem)
+    ) {
+      minElem = array[i];
+    }
+  }
+  return minElem;
+}
+// console.log(findClosestNumber([-4, -2, 1, 4, 8]));
