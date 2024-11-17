7d6abcffc2c62e1683aeb5a45783819a35edb746
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-18 04:00:04 +0500

    added countPrefixes function #57

--- easy/main.js
+++ easy/main.js
@@ -1145,20 +1145,20 @@ function findClosestNumber(array) {
 
 // 56) You are given a string s consisting of digits and an integer k.
 // A round can be completed if the length of s is greater than k. In one round, do the following:
-// Divide s into consecutive groups of size k such that the first k characters are in the first group, 
+// Divide s into consecutive groups of size k such that the first k characters are in the first group,
 // the next k characters are in the second group, and so on. Note that the size of the last group can be smaller than k.
-// Replace each group of s with a string representing the sum of all its digits. For example, 
-// "346" is replaced with "13" because 3 + 4 + 6 = 13. Merge consecutive groups together to form a new string. 
+// Replace each group of s with a string representing the sum of all its digits. For example,
+// "346" is replaced with "13" because 3 + 4 + 6 = 13. Merge consecutive groups together to form a new string.
 // If the length of the string is greater than k, repeat from step 1. Return s after all rounds have been completed.
 // Input: s = "11111222223", k = 3
 // Output: "135"
-// Explanation: 
+// Explanation:
 // - For the first round, we divide s into groups of size 3: "111", "112", "222", and "23".
-  // ​​​​​Then we calculate the digit sum of each group: 1 + 1 + 1 = 3, 1 + 1 + 2 = 4, 2 + 2 + 2 = 6, and 2 + 3 = 5. 
-  // So, s becomes "3" + "4" + "6" + "5" = "3465" after the first round.
+// ​​​​​Then we calculate the digit sum of each group: 1 + 1 + 1 = 3, 1 + 1 + 2 = 4, 2 + 2 + 2 = 6, and 2 + 3 = 5.
+// So, s becomes "3" + "4" + "6" + "5" = "3465" after the first round.
 // - For the second round, we divide s into "346" and "5".
-  // Then we calculate the digit sum of each group: 3 + 4 + 6 = 13, 5 = 5. 
-  // So, s becomes "13" + "5" = "135" after second round. 
+// Then we calculate the digit sum of each group: 3 + 4 + 6 = 13, 5 = 5.
+// So, s becomes "13" + "5" = "135" after second round.
 // Now, s.length <= k, so we return "135" as the answer.
 
 function digitSum(str, k) {
@@ -1177,3 +1177,24 @@ function digitSum(str, k) {
   return currentStr;
 }
 // console.log(digitSum("11111222223", 3));
+
+// 57) You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+// Return the number of strings in words that are a prefix of s. A prefix of a string is a substring 
+// that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+// Input: words = ["a","b","c","ab","bc","abc"], s = "abc"
+// Output: 3
+// Explanation:
+// The strings in words which are a prefix of s = "abc" are:
+// "a", "ab", and "abc".
+// Thus the number of strings in words which are a prefix of s is 3.
+
+function countPrefixes(array, str) {
+  let cnt = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == str.slice(0, array[i].length)) {
+      cnt++;
+    }
+  }
+  return cnt;
+}
+// console.log(countPrefixes(["a", "b", "c", "ab", "bc", "abc"], "abc"));
