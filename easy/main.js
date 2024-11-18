53b666fe701ae1ef530482e8d4e13a08231c26db
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-18 13:52:27 +0500

    added smallestEvenMultiple function #62

--- easy/main.js
+++ easy/main.js
@@ -1314,3 +1314,13 @@ function mostFrequentEven(nums) {
   return result;
 }
 // console.log(mostFrequentEven([0, 1, 2, 2, 4, 4, 1]));
+
+// 62) Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+// Input: n = 5
+// Output: 10
+// Explanation: The smallest multiple of both 5 and 2 is 10.
+
+function smallestEvenMultiple(num) {
+  return num % 2 === 0 ? num : num * 2;
+}
+// console.log(smallestEvenMultiple(6));
