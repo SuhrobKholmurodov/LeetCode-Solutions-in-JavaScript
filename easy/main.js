3427074939b50c651442754f60eebe2abda49c86
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-18 15:01:51 +0500

    added convertTemperature function #66

--- easy/main.js
+++ easy/main.js
@@ -1387,3 +1387,20 @@ function averageValue(array) {
   return 0;
 }
 // console.log(averageValue([1, 3, 6, 10, 12, 15]));
+
+// 66) You are given a non-negative floating point number rounded to two decimal places celsius, 
+// that denotes the temperature in Celsius. You should convert Celsius into Kelvin and Fahrenheit 
+// and return it as an array ans = [kelvin, fahrenheit]. Return the array ans. Answers within 
+// 10-5 of the actual answer will be accepted.
+// Kelvin = Celsius + 273.15;
+// Fahrenheit = Celsius * 1.8 + 32.0;
+// Input: celsius = 36.50
+// Output: [309.65000,97.70000]
+// Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+function convertTemperature(celsius) {
+  const kelvin = celsius + 273.15;
+  const fahrenheit = celsius * 1.8 + 32.0;
+  return [kelvin, fahrenheit]; 
+}
+// console.log(convertTemperature(36.5)); 
