d53cc6ae6291e096daded7019293a5c00cfb10cb
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-18 15:48:14 +0500

    added maximumValue function #69

--- easy/main.js
+++ easy/main.js
@@ -1453,3 +1453,26 @@ function isCircularSentence(str) {
   return true;
 }
 // console.log(isCircularSentence("Leetcode eisc cool"));
+
+// 69) The value of an alphanumeric string can be defined as:
+// The numeric representation of the string in base 10, if it comprises of digits only. The length of the string, otherwise.
+// Given an array strs of alphanumeric strings, return the maximum value of any string in strs.
+// Input: strs = ["alic3","bob","3","4","00000"]
+// Output: 5
+// Explanation:
+// - "alic3" consists of both letters and digits, so its value is its length, i.e. 5.
+// - "bob" consists only of letters, so its value is also its length, i.e. 3.
+// - "3" consists only of digits, so its value is its numeric equivalent, i.e. 3.
+// - "4" also consists only of digits, so its value is 4.
+// - "00000" consists only of digits, so its value is 0.
+// Hence, the maximum value is 5, of "alic3".
+
+function maximumValue(str) {
+  let max = 0;
+  for (let i = 0; i < str.length; i++) {
+    let value = isNaN(str[i]) ? str[i].length : parseInt(str[i]);
+    max = Math.max(max, value);
+  }
+  return max;
+}
+// console.log(maximumValue(["alic3", "bob", "3", "4", "00000"]));
