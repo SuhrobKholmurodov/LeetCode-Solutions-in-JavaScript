d7d8a7dc10a95f9ca734f2aa27db6484848088bf
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-18 16:56:01 +0500

    added maximumCount function #71

--- easy/main.js
+++ easy/main.js
@@ -1496,3 +1496,22 @@ function countDigits(num) {
   return cnt;
 }
 // console.log(countDigits(1248));
+
+// 71) Given an array nums sorted in non-decreasing order, return the maximum between the number
+// of positive integers and the number of negative integers. In other words, if the number of positive 
+// integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+// Note that 0 is neither positive nor negative.
+// Input: nums = [-2,-1,-1,1,2,3]
+// Output: 3
+// Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
+
+function maximumCount(array) {
+  let positiveCnt = 0;
+  let negativeCnt = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) positiveCnt++;
+    if (array[i] < 0) negativeCnt++;
+  }
+  return Math.max(positiveCnt, negativeCnt);
+}
+// console.log(maximumCount([-2, -1, -1, 1, 2, 3]));
