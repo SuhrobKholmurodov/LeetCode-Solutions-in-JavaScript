277333df926e898aab6f02f6bd49feb0b000a830
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-19 16:51:08 +0500

    added findDelayedArrivalTime function #80

--- easy/main.js
+++ easy/main.js
@@ -1704,3 +1704,16 @@ function sumOfMultiples(num) {
   return sum;
 }
 // console.log(sumOfMultiples(7));
+
+// 80) You are given a positive integer arrivalTime denoting the arrival time of a train in hours,
+// and another positive integer delayedTime denoting the amount of delay in hours.
+// Return the time when the train will arrive at the station.
+// Note that the time in this problem is in 24-hours format.
+// Input: arrivalTime = 15, delayedTime = 5
+// Output: 20
+// Explanation: Arrival time of the train was 15:00 hours. It is delayed by 5 hours. Now it will reach at 15+5 = 20 (20:00 hours).
+
+function findDelayedArrivalTime(arrivalTime, delayedTime) {
+  return (arrivalTime + delayedTime) % 24;
+}
+// console.log(findDelayedArrivalTime(15, 5));
