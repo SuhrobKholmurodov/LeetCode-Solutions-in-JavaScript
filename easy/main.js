76499ad705561231ed19bc550bcbf5bd4fcb6457
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-19 15:31:02 +0500

    added findTheArrayConcVal function #76

--- easy/main.js
+++ easy/main.js
@@ -1589,7 +1589,7 @@ function alternateDigitSum(num) {
 // For example, for the integer 10921, the separation of its digits is [1,0,9,2,1]
 // Input: nums = [13,25,83,77]
 // Output: [1,3,2,5,8,3,7,7]
-// Explanation: 
+// Explanation:
 // - The separation of 13 is [1,3].
 // - The separation of 25 is [2,5].
 // - The separation of 83 is [8,3].
@@ -1605,3 +1605,36 @@ function separateDigits(array) {
 }
 // console.log(separateDigits([13, 25, 83, 77]));
 
+// 76) You are given a 0-indexed integer array nums.
+// The concatenation of two numbers is the number formed by concatenating their numerals.
+// For example, the concatenation of 15, 49 is 1549.
+// The concatenation value of nums is initially equal to 0. Perform this operation until nums becomes empty:
+// If there exists more than one number in nums, pick the first element and last element in nums respectively and
+// add the value of their concatenation to the concatenation value of nums, then delete the first and last element from nums.
+// If one element exists, add its value to the concatenation value of nums, then delete it.
+// Return the concatenation value of the nums.
+// Input: nums = [7,52,2,4]
+// Output: 596
+// Explanation: Before performing any operation, nums is [7,52,2,4] and concatenation value is 0.
+//  - In the first operation:
+// We pick the first element, 7, and the last element, 4.
+// Their concatenation is 74, and we add it to the concatenation value, so it becomes equal to 74.
+// Then we delete them from nums, so nums becomes equal to [52,2].
+//  - In the second operation:
+// We pick the first element, 52, and the last element, 2.
+// Their concatenation is 522, and we add it to the concatenation value, so it becomes equal to 596.
+// Then we delete them from the nums, so nums becomes empty.
+// Since the concatenation value is 596 so the answer is 596.
+
+function findTheArrayConcVal(array) {
+  let res = [];
+  let length = Math.floor(array.length / 2);
+  for (let i = 0; i < length; i++) {
+    res.push(String(array[i]) + String(array[array.length - 1 - i]));
+  }
+  if (array.length % 2 !== 0) {
+    res.push(String(array[length]));
+  }
+  return res.map(Number).reduce((a, b) => a + b, 0);
+}
+// console.log(findTheArrayConcVal([7, 52, 2, 4]));
