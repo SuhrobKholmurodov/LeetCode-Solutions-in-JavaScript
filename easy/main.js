599fd0214df7808147c91bfec0c23404efb4e044
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-20 00:49:54 +0500

    added theMaximumAchievableX function #85

--- easy/main.js
+++ easy/main.js
@@ -1806,3 +1806,17 @@ function distanceTraveled(mainTank, additionalTank) {
   return distance;
 }
 // console.log(distanceTraveled(5, 10));
+
+// 85) Given two integers, num and t. A number is achievable if it can become equal to num after applying the following operation:
+// Increase or decrease the number by 1, and simultaneously increase or decrease num by 1.
+// Return the maximum achievable number after applying the operation at most t times.
+// Input: num = 4, t = 1
+// Output: 6
+// Explanation:
+// Apply the following operation once to make the maximum achievable number equal to num:
+// Decrease the maximum achievable number by 1, and increase num by 1
+
+function theMaximumAchievableX(num, t) {
+  return num + t * 2;
+}
+console.log(theMaximumAchievableX(4, 1));
