6a4da56aeed2a2812afce0f220c7a8030c2f8474
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-19 15:53:03 +0500

    added vowelStrings function #77

--- easy/main.js
+++ easy/main.js
@@ -1638,3 +1638,33 @@ function findTheArrayConcVal(array) {
   return res.map(Number).reduce((a, b) => a + b, 0);
 }
 // console.log(findTheArrayConcVal([7, 52, 2, 4]));
+
+// 77) You are given a 0-indexed array of string words and two integers left and right.
+// A string is called a vowel string if it starts with a vowel character and ends with a
+//  vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+// Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+// Input: words = ["are","amy","u"], left = 0, right = 2
+// Output: 2
+// Explanation:
+// - "are" is a vowel string because it starts with 'a' and ends with 'e'.
+// - "amy" is not a vowel string because it does not end with a vowel.
+// - "u" is a vowel string because it starts with 'u' and ends with 'u'.
+// The number of vowel strings in the mentioned range is 2.
+
+function vowelStrings(words, left, right) {
+  let cnt = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+  for (let i = left; i <= right; i++) {
+    if (
+      vowels.includes(words[i][0]) &&
+      vowels.includes(
+        words[i][words[i].length - 1] ||
+          (words[i].length === 1 && vowels.includes(words[i][0]))
+      )
+    ) {
+      cnt++;
+    }
+  }
+  return cnt;
+}
+// console.log(vowelStrings(["are", "amy", "u"], 0, 2));
