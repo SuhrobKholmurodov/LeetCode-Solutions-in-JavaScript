48052765222f333ca428636ea1be8442e5761c82
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-20 01:07:13 +0500

    added isGood function #86

--- easy/main.js
+++ easy/main.js
@@ -1819,4 +1819,44 @@ function distanceTraveled(mainTank, additionalTank) {
 function theMaximumAchievableX(num, t) {
   return num + t * 2;
 }
-console.log(theMaximumAchievableX(4, 1));
+// console.log(theMaximumAchievableX(4, 1));
+
+// 86) You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+// base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which 
+// contains 1 to n - 1 exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+// Return true if the given array is good, otherwise return false.
+// Note: A permutation of integers represents an arrangement of these numbers.
+// Input: nums = [2, 1, 3]
+// Output: false
+// Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a 
+// permutation of base[n], is n = 3. However, base[3] has four elements but array nums has three. Therefore, 
+// it can not be a permutation of base[3] = [1, 2, 3, 3]. So the answer is false.
+
+function isGood(array) {
+  let maxElem = Math.max(...array);
+  let goodArray = true;
+  if (array.length !== maxElem + 1) {
+    return false;
+  }
+  for (let i = 1; i <= maxElem; i++) {
+    let count = 0;
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === i) {
+        count++;
+      }
+    }
+    if (i === maxElem) {
+      if (count !== 2) {
+        goodArray = false;
+        break;
+      }
+    } else {
+      if (count !== 1) {
+        goodArray = false;
+        break;
+      }
+    }
+  }
+  return goodArray;
+}
+// console.log(isGood([1, 3, 3, 2]));
