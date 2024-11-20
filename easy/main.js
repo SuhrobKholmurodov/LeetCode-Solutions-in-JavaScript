09391957c57586063fc1e6d3a65957f14e7381b4
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-20 13:50:48 +0500

    added isPrefixOfWord function #88

--- easy/main.js
+++ easy/main.js
@@ -1884,4 +1884,24 @@ function numberOfEmployeesWhoMetTarget(hours, target) {
   }
   return cnt;
 }
-console.log(numberOfEmployeesWhoMetTarget([0, 1, 2, 3, 4], 2));
+// console.log(numberOfEmployeesWhoMetTarget([0, 1, 2, 3, 4], 2));
+
+// 88) Given a sentence that consists of some words separated by a single space, and a searchWord,
+// check if searchWord is a prefix of any word in sentence. Return the index of the word in sentence
+// (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word,
+// return the index of the first word (minimum index). If there is no such word return -1.
+// A prefix of a string s is any leading contiguous substring of s.
+// Input: sentence = "i love eating burger", searchWord = "burg"
+// Output: 4
+// Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+
+function isPrefixOfWord(str, pref) {
+  let strSplit = str.split(" ");
+  for (let i = 0; i < strSplit.length; i++) {
+    if (strSplit[i].startsWith(pref)) {
+      return i + 1;
+    }
+  }
+  return -1;
+}
+// console.log(isPrefixOfWord("i love eating burger", "burg"));
