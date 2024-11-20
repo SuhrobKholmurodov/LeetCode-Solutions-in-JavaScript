8e104c18817b090d59ca0eaa732850a1660ccf9f
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-20 14:05:37 +0500

    added maxProduct function #89

--- easy/main.js
+++ easy/main.js
@@ -1905,3 +1905,28 @@ function isPrefixOfWord(str, pref) {
   return -1;
 }
 // console.log(isPrefixOfWord("i love eating burger", "burg"));
+
+// 89) Given the array of integers nums, you will choose two different indices i and j of that array.
+// Return the maximum value of (nums[i]-1)*(nums[j]-1).
+// Input: nums = [3,4,5,2]
+// Output: 12
+// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value,
+// that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12.
+
+function maxProduct(array) {
+  let sum = 0;
+  let maxElem = array[0];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      sum = (array[i] - 1) * (array[j] - 1);
+      if (sum > maxElem) {
+        maxElem = sum;
+      }
+      if (array[i] === array[j] && array[j] === maxElem) {
+        maxElem = 0;
+      }
+    }
+  }
+  return maxElem;
+}
+// console.log(maxProduct([3, 4, 5, 2]));
