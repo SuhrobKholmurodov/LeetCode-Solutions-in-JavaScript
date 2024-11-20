c63d4461fe7dbf42e5201cb2c03e7d24e3bed4b9
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-21 00:50:56 +0500

    added areNumbersAscending function #97

--- easy/main.js
+++ easy/main.js
@@ -2087,3 +2087,31 @@ function countKDifference(array, k) {
   return cnt;
 }
 // console.log(countKDifference([1, 2, 2, 1], 1));
+
+// 97) A sentence is a list of tokens separated by a single space with no leading or trailing spaces. 
+// Every token is either a positive number consisting of digits 0-9 with no leading zeros, 
+// or a word consisting of lowercase English letters. For example, "a puppy has 2 eyes 4 legs"
+//  is a sentence with seven tokens: "2" and "4" are numbers and the other tokens such as "puppy" are words.
+// Given a string s representing a sentence, you need to check if all the numbers in s are strictly increasing
+//  from left to right (i.e., other than the last number, each number is strictly smaller than the number on its right in s).
+// Return true if so, or false otherwise.
+// Input: s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles"
+// Output: true
+// Explanation: The numbers in s are: 1, 3, 4, 6, 12.
+// They are strictly increasing from left to right: 1 < 3 < 4 < 6 < 12.
+
+function areNumbersAscending(str) {
+  let strSplit = str.split(" ");
+  let filterNum = strSplit.filter((el, i) => {
+    const num = parseInt(el);
+    return !isNaN(num);
+  });
+  let toNumElem = filterNum.map(Number);
+  for (let i = 0; i < toNumElem.length - 1; i++) {
+    if (toNumElem[i] >= toNumElem[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(areNumbersAscending("1 box has 3 blue 4 red 6 green and 12 yellow marbles"));
