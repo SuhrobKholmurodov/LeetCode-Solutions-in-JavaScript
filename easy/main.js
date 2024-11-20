befb26c2ce0c9772da938736c221327d3065cfe7
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-20 14:43:42 +0500

    added runningSum function #90

--- easy/main.js
+++ easy/main.js
@@ -1930,3 +1930,25 @@ function maxProduct(array) {
   return maxElem;
 }
 // console.log(maxProduct([3, 4, 5, 2]));
+
+// 90) Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4]
+
+function runningSum(array) {
+  let sum = 0;
+  let res = [];
+  let sumArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let sliced = array.slice(0, i + 1);
+    res.push(sliced);
+  }
+  for (let i = 0; i < res.length; i++) {
+    sum = res[i].reduce((a, b) => a + b, 0);
+    sumArray.push(sum);
+  }
+  return sumArray;
+}
+// console.log(runningSum([1, 2, 3, 4]));
