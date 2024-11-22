b9d078c50db76cf593e1795033496c2dc6528c62
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-22 15:09:28 +0500

    added isBalanced function #99

--- easy/main.js
+++ easy/main.js
@@ -2145,3 +2145,26 @@ function minimumSum(num) {
   return parseInt(num1) + parseInt(num2);
 }
 // console.log(minimumSum(2932));
+
+// 99)You are given a string num consisting of only digits.
+// A string of digits is called balanced if the sum of the digits at even indices is equal to the sum of digits at odd indices.
+// Return true if num is balanced, otherwise return false.
+// Input: num = "1234"
+// Output: false
+// Explanation:
+// The sum of digits at even indices is 1 + 3 == 4, and the sum of digits at odd indices is 2 + 4 == 6.
+// Since 4 is not equal to 6, num is not balanced.
+
+function isBalanced(num) {
+  let evenCnt = 0;
+  let oddCnt = 0;
+  for (let i = 0; i < num.length; i++) {
+    if ([i] % 2 === 0) {
+      evenCnt += num[i] * 1;
+    } else {
+      oddCnt += num[i] * 1;
+    }
+  }
+  return evenCnt == oddCnt;
+}
+// console.log(isBalanced("1234"));
