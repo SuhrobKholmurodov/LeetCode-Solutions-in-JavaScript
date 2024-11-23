50ffcb33b2569457cbdedd8a3497828f312cb9d3
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-24 01:30:29 +0500

    added isLongPressedName function #100 ))

--- easy/main.js
+++ easy/main.js
@@ -2168,3 +2168,24 @@ function isBalanced(num) {
   return evenCnt == oddCnt;
 }
 // console.log(isBalanced("1234"));
+
+// 100)Your friend is typing his name into a keyboard. Sometimes, when typing a character c,
+// the key might get long pressed, and the character will be typed 1 or more times.
+// You examine the typed characters of the keyboard. Return True if it is possible that it was
+// your friends name, with some characters (possibly none) being long pressed.
+// Input: name = "alex", typed = "aaleex"
+// Output: true
+// Explanation: 'a' and 'e' in 'alex' were long pressed.
+
+function isLongPressedName(name, typed) {
+  let i = 0;
+  for (let j = 0; j < typed.length; j++) {
+    if (i < name.length && name[i] === typed[j]) {
+      i++;
+    } else if (j === 0 || typed[j] !== typed[j - 1]) {
+      return false;
+    }
+  }
+  return i === name.length;
+}
+// console.log(isLongPressedName("alex", "aaleex"));
