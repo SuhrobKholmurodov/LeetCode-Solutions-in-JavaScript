4e06d35098fd0ccaeb81823ef83fc805edfccecf
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-25 17:39:42 +0500

    added groupAnagrams function #11

--- medium/main.js
+++ medium/main.js
@@ -221,3 +221,24 @@ function lengthOfLongestSubstring(str) {
   return maxLen;
 }
 // console.log(lengthOfLongestSubstring("abcabcbb"));
+
+// 11) Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Explanation:
+// There is no string in strs that can be rearranged to form "bat".
+// The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+// The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+function groupAnagrams(array) {
+  let res = {};
+  for (let i = 0; i < array.length; i++) {
+    let key = array[i].toLowerCase().split("").sort().join("");
+    if (!res[key]) {
+      res[key] = [];
+    }
+    res[key].push(array[i]);
+  }
+  return Object.values(res);
+}
+// console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
