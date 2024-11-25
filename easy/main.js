5a03b05157cdf363f178fa84ff715301a0b737a7
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-25 13:20:45 +0500

    added findDifference function #103

--- easy/main.js
+++ easy/main.js
@@ -2225,3 +2225,32 @@ function smallestEqual(array) {
   return -1;
 }
 // console.log(smallestEqual([0, 1, 2]));
+
+// 103) Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+// answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+// answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+// Note that the integers in the lists may be returned in any order.
+// Input: nums1 = [1,2,3], nums2 = [2,4,6]
+// Output: [[1,3],[4,6]]
+// Explanation:
+// For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 
+// are not present in nums2. Therefore, answer[0] = [1,3].
+// For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 
+// are not present in nums2. Therefore, answer[1] = [4,6].
+
+function findDifference(nums1, nums2) {
+  let diff1 = [];
+  let diff2 = [];
+  for (let i = 0; i < nums1.length; i++) {
+    if (!nums2.includes(nums1[i]) && !diff1.includes(nums1[i])) {
+      diff1.push(nums1[i]);
+    }
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    if (!nums1.includes(nums2[i]) && !diff2.includes(nums2[i])) {
+      diff2.push(nums2[i]);
+    }
+  }
+  return [diff1, diff2];
+}
+// console.log(findDifference([1, 2, 3, 3], [1, 1, 2, 2]));
