c2124288e87d035b87dd90e2c3045323305b1b3e
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-11-27 16:37:16 +0500

    added reverseWords function #13

--- medium/main.js
+++ medium/main.js
@@ -273,3 +273,19 @@ function searchRange(array, target) {
   return [-1, -1];
 }
 // console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
+
+// 13) Given an input string s, reverse the order of the words.
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+// Return a string of the words in reverse order concatenated by a single space.
+// Note that s may contain leading or trailing spaces or multiple spaces between two words.
+// The returned string should only have a single space separating the words. Do not include any extra spaces.
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+function reverseWords(str) {
+  let words = str.split(" ").filter((word) => word !== "");
+  let reversedWords = words.reverse();
+  return reversedWords.join(" ");
+}
+// console.log(reverseWords("a good   example"));
