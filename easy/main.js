82e9c42928afbbec2527b6ae4a568989d44d133a
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-12-03 09:04:14 +0500

    added mostCommonWord function #113

--- easy/main.js
+++ easy/main.js
@@ -2445,3 +2445,32 @@ function pivotIndex(array) {
   return res;
 }
 // console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+
+// 113) Given a string paragraph and a string array of the banned words banned, return the most frequent
+// word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+// The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+// Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+// Output: "ball"
+// Explanation:
+// "hit" occurs 3 times, but it is a banned word.
+// "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph.
+// Note that words in the paragraph are not case sensitive,
+// that punctuation is ignored (even if adjacent to words, such as "ball,"),
+// and that "hit" isn't the answer even though it occurs more because it is banned
+
+function mostCommonWord(str, banned) {
+  let wordCount = {};
+  let maxWord = "";
+  let maxCount = 0;
+  let filtered = str.split(/[^a-zA-Z]+/).map((el) => el.toLowerCase());
+  let validWords = filtered.filter((el) => el && !banned.includes(el));
+  for (let i = 0; i < validWords.length; i++) {
+    wordCount[validWords[i]] = (wordCount[validWords[i]] || 0) + 1;
+    if (wordCount[validWords[i]] > maxCount) {
+      maxCount = wordCount[validWords[i]];
+      maxWord = validWords[i];
+    }
+  }
+  return maxWord || -1;
+}
+// console.log(mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit",]));
