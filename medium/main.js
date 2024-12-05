6d791120b2828c209618cbd6d8879cba58d994ea
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-12-05 09:09:34 +0500

    added findClosestElements function #17

--- medium/main.js
+++ medium/main.js
@@ -369,3 +369,17 @@ function nthUglyNumber(n) {
   return uglyNumbers[n - 1];
 }
 // console.log(nthUglyNumber(1407));
+
+// 17) Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. 
+// The result should also be sorted in ascending order. An integer a is closer to x than an integer b if:
+// |a - x| < |b - x|, or
+// |a - x| == |b - x| and a < b
+// Input: arr = [1,2,3,4,5], k = 4, x = 3
+// Output: [1,2,3,4]
+
+function findClosestElements(array, k, x) {
+  const sorted = array.sort((a, b) => Math.abs(a - x) - Math.abs(b - x));
+  const res = sorted.slice(0, k);
+  return res.sort((a, b) => a - b);
+}
+// console.log(findClosestElements([1, 2, 3, 4, 5], 4, 3));
