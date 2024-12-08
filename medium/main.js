488c53c320a4317a85873e11160a4db0b77c9f0c
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-12-08 12:23:01 +0500

    added findPairs function #20

--- medium/main.js
+++ medium/main.js
@@ -435,3 +435,30 @@ function maxDistance(arrays) {
   return maxDiff;
 }
 // console.log(maxDistance([[1, 2, 3], [4, 5], [1, 2, 3],]));
+
+// 20) Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+// A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+// 0 <= i, j < nums.length
+// i != j
+// |nums[i] - nums[j]| == k
+// Notice that |val| denotes the absolute value of val.
+// Input: nums = [3,1,4,1,5], k = 2
+// Output: 2
+// Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+// Although we have two 1s in the input, we should only return the number of unique pairs.
+
+function findPairs(nums, k) {
+  let cnt = 0;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) === k) {
+        cnt++;
+        break;
+      }
+    }
+  }
+  return cnt;
+}
+// console.log(findPairs([3, 1, 4, 1, 5], 2));
