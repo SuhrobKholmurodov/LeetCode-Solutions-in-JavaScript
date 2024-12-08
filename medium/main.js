e8e673d3bba5fad33ff60365336b63eebe5b8b13
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-12-09 00:38:28 +0500

    added getKth function #24

--- medium/main.js
+++ medium/main.js
@@ -535,3 +535,46 @@ function strWithout3a3b(a, b) {
   return result.join("");
 }
 // console.log(strWithout3a3b(1, 2));
+
+// 24) The power of an integer x is defined as the number of steps needed to transform x into 1 using the following steps:
+// if x is even then x = x / 2
+// if x is odd then x = 3 * x + 1
+// For example, the power of x = 3 is 7 because 3 needs 7 steps to become 1 (3 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1).
+// Given three integers lo, hi and k. The task is to sort all integers in the interval [lo, hi] by the power value in ascending order,
+//  if two or more integers have the same power value sort them by ascending order.
+// Return the kth integer in the range [lo, hi] sorted by the power value.
+// Notice that for any integer x (lo <= x <= hi) it is guaranteed that x will transform into 1 using 
+// these steps and that the power of x is will fit in a 32-bit signed integer.
+// Input: lo = 12, hi = 15, k = 2
+// Output: 13
+// Explanation: The power of 12 is 9 (12 --> 6 --> 3 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1)
+// The power of 13 is 9
+// The power of 14 is 17
+// The power of 15 is 17
+// The interval sorted by the power value [12,13,14,15]. For k = 2 answer is the second element which is 13.
+// Notice that 12 and 13 have the same power value and we sorted them in ascending order. Same for 14 and 15.
+
+function getKth(lo, hi, k) {
+  let res = [];
+  for (let i = lo; i <= hi; i++) {
+    let currElem = i;
+    let povValue = 0;
+    while (currElem !== 1) {
+      if (currElem % 2 === 0) {
+        currElem /= 2;
+      } else {
+        currElem = currElem * 3 + 1;
+      }
+      povValue++;
+    }
+    res.push({ num: i, steps: povValue });
+  }
+  res.sort((a, b) => {
+    if (a.steps !== b.steps) {
+      return a.steps - b.steps;
+    }
+    return a.num - b.num;
+  });
+  return res[k - 1].num;
+}
+// console.log(getKth(12, 15, 2));
