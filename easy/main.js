f5daca1b0942e534b9b0d9bef97e9722218107ec
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-12-16 13:12:22 +0500

    added addStrings function

--- easy/main.js
+++ easy/main.js
@@ -2532,3 +2532,14 @@ function guessNumber(n) {
   }
 }
 // console.log(guessNumber(10));
+
+// 117) Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). 
+// You must also not convert the inputs to integers directly.
+// Input: num1 = "11", num2 = "123"
+// Output: "134"
+
+function addStrings(num1, num2) {
+  return (BigInt(num1) + BigInt(num2)).toString();
+}
+// console.log(addStrings('9333852702227987', '85731737104263')); 
