08c4f3d1ea3221dabad11f9131ca5c6151165066
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-12-20 16:07:16 +0500

    added findMedianSortedArrays function #2

--- hard/main.js
+++ hard/main.js
@@ -15,3 +15,20 @@ function firstMissingPositive(array) {
   return filtered[filtered.length - 1] + 1;
 }
 // console.log(firstMissingPositive([3, 4, -1, 1]));
+
+// 2) Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+// The overall run time complexity should be O(log (m+n)).
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5
+
+function findMedianSortedArrays(array1, array2) {
+  let concats = array1.concat(array2).sort((a, b) => a - b);
+  let length = concats.length;
+  if (length % 2 === 0) {
+    return concats.slice(length / 2 - 1, length / 2 + 1).reduce((a, b) => a + b) / 2;
+  } else {
+    return concats[Math.floor(length / 2)];
+  }
+}
+// console.log(findMedianSortedArrays([1, 2], [3, 4]));
