0ce1a034fbff7daeff1af69164725ade3acc3f43
Kholmurodov <suhrobkholmurodov1@gmail.com>
2024-12-20 17:21:32 +0500

    added letterCombinations function #32

--- medium/main.js
+++ medium/main.js
@@ -730,3 +730,39 @@ function longestPalindrome(str) {
   return res;
 }
 // console.log(longestPalindrome('babad'));
+
+// 32) Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+// Return the answer in any order. A mapping of digits to letters (just like on the telephone buttons) is given below. 
+// Note that 1 does not map to any letters.
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+function letterCombinations(digits) {
+  const digitToLetters = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  };
+  let res = [''];
+  if (digits.length === 0) {
+    return [];
+  }
+
+  for (let i = 0; i < digits.length; i++) {
+    const letters = digitToLetters[digits[i]];
+    const temp = [];
+    for (let j = 0; j < letters.length; j++) {
+      for (let k = 0; k < res.length; k++) {
+        temp.push(res[k] + letters[j]);
+      }
+    }
+    res = temp;
+  }
+  return res;
+}
+// console.log(letterCombinations('23'));
